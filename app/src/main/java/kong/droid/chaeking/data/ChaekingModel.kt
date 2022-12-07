8ad02d5bdb2601eb17fb4dee615b6b3b159d5587
eunie9498 <bvegemilb@naver.com>
2022-12-07 22:35:17 +0900

    :zap: DTO, SharedHelpder

---
+++ app/src/main/java/kong/droid/chaeking/data/ChaekingModel.kt
@@ -0,0 +1,270 @@
+package kong.droid.chaeking.data
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Users(
+    val email: String? = "",
+    val pw: String? = ""
+) : Parcelable
+
+@Parcelize
+data class HomeResponse(
+    var result: String? = "",
+    var reason: String? = "",
+    var data: HomeData? = null
+) : Parcelable
+
+@Parcelize
+data class HomeData(
+    var best_seller: List<HomeBestSeller>? = null,
+    var book_analysis: HomeBookContents? = null,
+    var nickname: String? = ""
+) : Parcelable
+
+@Parcelize
+data class HomeBookContents(
+    var contents: List<HOME.HomeBookContent>,
+    var total_amount: Int,
+    var type: String
+) : Parcelable
+
+sealed class HOME {
+    @Parcelize
+    data class HomeBookContent(
+        var amount: Int,
+        var name: String,
+    ) : Parcelable, HOME()
+
+    @Parcelize
+    data class Nick(
+        var nickName: String?
+    ) : Parcelable, HOME()
+
+    object Badge : HOME()
+
+    @Parcelize
+    data class HomeGraph(
+        var name: String,
+        var amt: Int
+    ) : Parcelable, HOME()
+
+    @Parcelize
+    data class Recomm(
+        val data: List<HomeBestSeller>? = null
+    ) : Parcelable, HOME()
+
+    @Parcelize
+    data class Analysis(
+        var data: HomeBookContents
+    ) : Parcelable, HOME()
+}
+
+
+@Parcelize
+data class BookData(
+    val authors: String? = "",
+    var id: Int,
+    var image_url: String? = "",
+    var name: String? = "",
+) : Parcelable
+
+@Parcelize
+data class HomeBestSeller(
+    var authors: String? = "",
+    var id: Int? = 0,
+    var image_url: String? = "",
+    var name: String? = "",
+    var publisher: String? = ""
+) : Parcelable
+
+@Parcelize
+data class JoinBody(
+    var email: String? = "",
+    var nickname: String? = "",
+    var password: String? = "",
+    var secret_key: String? = "",
+    var sex: String? = ""
+) : Parcelable
+
+@Parcelize
+data class LoginBody(
+    var email: String? = "",
+    var password: String? = "",
+    var secret_key: String? = "",
+) : Parcelable
+
+@Parcelize
+data class ResultResponse(
+    var result: String? = "",
+    var reason: String? = ""
+) : Parcelable
+
+@Parcelize
+data class LoginResponse(
+    var result: String? = "",
+    var reason: String? = "",
+    var data: TokenData? = null
+) : Parcelable
+
+@Parcelize
+data class TokenData(
+    var access_token: String? = "",
+    var refresh_token: String? = ""
+) : Parcelable
+
+@Parcelize
+data class GetBookDetailResponse(
+    val result: String? = "",
+    val reason: String? = "",
+    val data: BookDetailItem? = null
+) : Parcelable
+
+@Parcelize
+data class GetBookResponse(
+    val result: String? = "",
+    val reason: String? = "",
+    val data: List<GetBookData>? = null
+) : Parcelable
+
+@Parcelize
+data class GetBookData(
+    val authors: String? = "",
+    var id: Int? = 0,
+    var image_url: String? = "",
+    var name: String? = "",
+    var publisher: String? = ""
+) : Parcelable
+
+@Parcelize
+data class BookDetailItem(
+    val authors: List<String>,
+    val detail_info: String,
+    val id: Int,
+    val image_url: String,
+    val isbn: String,
+    val name: String,
+    val price: Int,
+    val publication_date: String,
+    val publisher: String
+) : Parcelable
+
+@Parcelize
+data class TagResponse(
+    var result: String,
+    var reason: String? = "",
+    var data: List<TagItem>? = null
+) : Parcelable
+
+@Parcelize
+data class TagItem(
+    var id: Int,
+    var name: String,
+    var selected: Boolean? = false
+) : Parcelable
+
+@Parcelize
+data class SearchBookResponse(
+    var result: String,
+    var reason: String? = "",
+    var data: List<SearchBookItem>
+) : Parcelable
+
+@Parcelize
+data class SearchBookItem(
+    var authors: String,
+    var id: Int,
+    var name: String,
+    var publisher: String
+) : Parcelable
+
+@Parcelize
+data class WishBookResponse(
+    var result: String?,
+    var reason: String?,
+    var data: List<WishBookData>
+) : Parcelable
+
+@Parcelize
+data class WishBookData(
+    var book_id: Int,
+    var memo: String? = ""
+) : Parcelable
+
+@Parcelize
+data class MemoBody(
+    var memo: String
+) : Parcelable
+
+@Parcelize
+data class AlreadyBookItem(
+    var book_id: Int,
+    var memo: String? = "",
+    var rate: Int,
+    var tag_ids: List<Int>
+) : Parcelable
+
+@Parcelize
+data class GetAlreadyBookResponse(
+    var result: String?,
+    var reason: String?,
+    var data: List<GetAlreadyBookItem>? = ArrayList()
+) : Parcelable
+
+@Parcelize
+data class GetAlreadyBookItem(
+    var book_name: String,
+    var book_id: Int,
+    var image_url: String? = "",
+    var id: Int
+) : Parcelable
+
+@Parcelize
+data class EditAlreadyBookBody(
+    var memo: String,
+    var rate: Int,
+    var tag_ids: List<Int>
+) : Parcelable
+
+@Parcelize
+data class UserData(
+    var email: String,
+    var image_url: String? = "",
+    var nickname: String,
+    var night_push: Boolean? = false,
+    var push: Boolean? = false
+) : Parcelable
+
+@Parcelize
+data class UserDataResponse(
+    var result: String,
+    var reason: String? = "",
+    var data: UserData? = null
+) : Parcelable
+
+@Parcelize
+data class ShelfResponse(
+    var data: List<SHELF.UserShelfData>? = null,
+    var has_next: Boolean,
+    var reason: String,
+    var result: String,
+    var total_count: Int
+) : Parcelable
+
+sealed class SHELF {
+    @Parcelize
+    data class ShelfTop(
+        var count: Int
+    ) : Parcelable, SHELF()
+
+    @Parcelize
+    data class UserShelfData(
+        var book_id: Int,
+        var book_name: String,
+        var id: Int,
+        var image_url: String,
+        var memo: String,
+        var rate: Int
+    ) : Parcelable, SHELF()
+}
---
+++ app/src/main/java/kong/droid/chaeking/data/SharedHelper.kt
@@ -0,0 +1,83 @@
+package kong.droid.chaeking.data
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Base64
+import androidx.preference.PreferenceManager
+import java.nio.charset.Charset
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
+
+class SharedHelper(val context: Context) {
+
+    var sharedPreferences: SharedPreferences =
+        context.getSharedPreferences(Constants.SHARED, Context.MODE_PRIVATE)
+
+    fun addPreference(key: String, value: Any?) {
+        sharedPreferences.edit().apply {
+            when (value) {
+                is Int -> putInt(key, value).apply()
+                is Boolean -> putBoolean(key, value).apply()
+                is String -> putString(key, value).apply()
+                is Float -> putFloat(key, value).apply()
+                is Long -> putLong(key, value).apply()
+                else -> return
+            }
+        }
+    }
+
+    fun deletePreference(key: String) {
+        val systemPres: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+        val editor = systemPres.edit()
+        editor.remove(key)
+        editor.apply()
+    }
+
+    val getFbToken: String
+        get() = sharedPreferences.getString(Constants.Shared.FB_TOKEN, "") ?: ""
+
+    val getAccessToken: String
+        get() = sharedPreferences.getString(Constants.Shared.ACCESS_TOKEN, "") ?: ""
+
+    val getRefreshToken: String
+        get() = sharedPreferences.getString(Constants.Shared.REFRESH_TOKEN, "") ?: ""
+
+    val getUserNick: String
+        get() = sharedPreferences.getString(Constants.Shared.USER_NICK, "") ?: ""
+
+    val getUserEmail: String
+        get() = sharedPreferences.getString(Constants.Shared.USER_EMAIL, "") ?: ""
+
+    val getUserProfile: String
+        get() = sharedPreferences.getString(Constants.Shared.USER_PROFILE, "") ?: ""
+
+    fun newEncrypt(
+        input: ByteArray,
+    ): String {
+        return newEncryptUtil(Cipher.ENCRYPT_MODE, input)
+    }
+
+    fun newDecrypt(
+        input: ByteArray,
+    ): String {
+        return newEncryptUtil(Cipher.DECRYPT_MODE, input)
+    }
+
+    private fun newEncryptUtil(mode: Int, input: ByteArray): String {
+        return try {
+            val key = (Constants.CHAEKING + getFbToken).substring(0, 32).toByteArray(Charset.defaultCharset())
+            val iv = getFbToken.substring(0, 16).toByteArray(Charset.defaultCharset())
+            val secretKeySpec = SecretKeySpec(key, "AES")
+            val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+            cipher.init(mode, secretKeySpec, IvParameterSpec(iv))
+            cipher.doFinal(input)
+            val cipherText = cipher.doFinal(input)
+            Base64.encodeToString(cipherText, Base64.NO_WRAP)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            ""
+        }
+    }
+}
